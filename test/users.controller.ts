4f66c033e68187ce6f2b472c892b3aac008b83d3
Amy Troschinetz <amy@lexicalunit.com>
2020-11-15 20:23:22 -0800

    Updates tests

--- test/users.controller.ts
+++ test/users.controller.ts
@@ -9,7 +9,7 @@ describe('User', () => {
       .get('/api/v1/users')
       .expect('Content-Type', /json/)
       .then((r) => {
-        expect(r.body).to.be.an('array').of.length(2);
+        expect(r.body).to.be.an('array').of.length(1);
       }));
 
   it('should add a new user', () =>
@@ -21,11 +21,55 @@ describe('User', () => {
         expect(r.body).to.be.an('object').that.has.property('name').equal('test');
       }));
 
-  it('should get an user by id', () =>
+  it('should get a user by id', () =>
     request(Server)
-      .get('/api/v1/users/2')
+      .get('/api/v1/users/c558d991-8c8e-40d1-af48-2d680d8cd075')
       .expect('Content-Type', /json/)
       .then((r) => {
-        expect(r.body).to.be.an('object').that.has.property('name').equal('test');
+        expect(r.body).to.be.an('object').that.has.property('name').equal('Amy Troschinetz');
+      }));
+
+  it('should update a user by id', () =>
+    request(Server)
+      .put('/api/v1/users/c558d991-8c8e-40d1-af48-2d680d8cd075')
+      .send({ name: 'Just Amy' })
+      .expect('Content-Type', /json/)
+      .then((r) => {
+        expect(r.body).to.be.an('object').that.has.property('name').equal('Just Amy');
+      }));
+
+  it('should get a user favorites by id', () =>
+    request(Server)
+      .get('/api/v1/users/c558d991-8c8e-40d1-af48-2d680d8cd075/favorites')
+      .expect('Content-Type', /json/)
+      .then((r) => {
+        expect(r.body).to.be.an('array').of.length(1);
+      }));
+
+  it('should add a new favorite', () =>
+    request(Server)
+      .post('/api/v1/favorites')
+      .send({ link: 'http://example.com', user_id: 'c558d991-8c8e-40d1-af48-2d680d8cd075' })
+      .expect('Content-Type', /json/)
+      .then((r) => {
+        expect(r.body).to.be.an('object').that.has.property('link').equal('http://example.com');
+      }));
+
+  it('should get a favorite by id', () =>
+    request(Server)
+      .get('/api/v1/favorites/43da97e3-b219-4215-b31c-16171c413b49')
+      .expect('Content-Type', /json/)
+      .then((r) => {
+        expect(r.body)
+          .to.be.an('object')
+          .that.has.property('link')
+          .equal('https://www.reddit.com/r/hognosesnakes/');
+      }));
+
+  it('should delete a favorite by id', () =>
+    request(Server)
+      .delete('/api/v1/favorites/43da97e3-b219-4215-b31c-16171c413b49')
+      .then((r) => {
+        expect(r.status).to.eq(204);
       }));
 });
